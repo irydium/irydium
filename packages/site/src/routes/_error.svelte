0dd67a411bc3ef5db4ec2c13b0ff41c72762a096
William Lachance <wrlach@gmail.com>
2021-01-02 21:20:00 -0500

    Prettier fixes

--- packages/site/src/routes/_error.svelte
+++ packages/site/src/routes/_error.svelte
@@ -1,34 +1,35 @@
 <script lang="ts">
-	export let status: number;
-	export let error: Error;
+  export let status: number;
+  export let error: Error;
 
-	const dev = process.env.NODE_ENV === 'development';
+  const dev = process.env.NODE_ENV === "development";
 </script>
 
 <style>
-	h1, p {
-		margin: 0 auto;
-	}
+  h1,
+  p {
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+    margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>{status}</title>
+  <title>{status}</title>
 </svelte:head>
 
 <h1>{status}</h1>
@@ -36,5 +37,5 @@
 <p>{error.message}</p>
 
 {#if dev && error.stack}
-	<pre>{error.stack}</pre>
+  <pre>{error.stack}</pre>
 {/if}
