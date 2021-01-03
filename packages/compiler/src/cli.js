70529e4ae6ddc07f2b6a4b832f67951b533b9b1b
William Lachance <wrlach@gmail.com>
2021-01-03 18:30:52 -0500

    Fail gracefully if no file provided to compiler

--- packages/compiler/src/cli.js
+++ packages/compiler/src/cli.js
@@ -3,5 +3,9 @@ import { compile } from "./compile";
 const fs = require("fs");
 
 const args = process.argv.slice(2);
+if (args.length !== 1) {
+  console.error(`Usage: ${process.argv[1]} <irmd file>`);
+  process.exit(1);
+}
 const input = fs.readFileSync(args[0]);
 compile(input).then((output) => console.log(output));
