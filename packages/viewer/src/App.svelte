88870dce0f3833a5341df68e2bbf84379014b4f7
William Lachance <wrlach@gmail.com>
2021-03-06 08:30:35 -0500

    Debug viewer WIP (#3)

---
+++ packages/viewer/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<style>
+  iframe {
+    border: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+  }
+
+  .header {
+    z-index: 1000;
+  }
+
+  .buttons {
+    position: fixed;
+    right: 20px;
+    top: 10px;
+  }
+</style>
+
+<div class="header">
+  <div class="buttons">
+    <button>Graph</button>
+    <button>Debug</button>
+  </div>
+</div>
+
+<iframe title="irydium" src="/iridium"></iframe>
--- packages/viewer/src/cli.js
+++ packages/viewer/src/cli.js
@@ -1,6 +1,13 @@
 import { compile } from "../../compiler/src/compile.js";
 
+// the following two files are imported as strings, this lets us
+// return them literally
+import bundlecss from "../build/bundle.css";
+import bundlejs from "../build/bundle.js";
+import index from "./index.html";
+
 const livereload = require("livereload");
+const mustache = require("mustache");
 const polka = require("polka");
 const fs = require("fs");
 const path = require("path");
@@ -11,12 +18,50 @@ if (args.length !== 1) {
   process.exit(1);
 }
 
-const server = livereload.createServer({ extraExts: ["irmd"] });
-server.watch(path.dirname(args[0]));
+const irmdReloadServer = livereload.createServer({ extraExts: ["irmd"] });
+irmdReloadServer.watch([path.dirname(args[0])]);
+
+// only add logic to reload the dev server for non-production cases
+const BUILD_DIR = __dirname + "/../build";
+const BUILD_LIVERELOAD_PORT = 35730;
+let buildReloadServer;
+if (!__PRODUCTION__) {
+  buildReloadServer = livereload.createServer({
+    port: BUILD_LIVERELOAD_PORT,
+  });
+  buildReloadServer.watch([BUILD_DIR]);
+}
+
+function getFileContents(filename) {
+  return fs.readFileSync(filename);
+}
 
 polka()
-  .get("/", async (req, res) => {
-    const input = fs.readFileSync(args[0]);
+  .get("/", async (_, res) => {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.end(
+      mustache.render(index, {
+        liveReload: !__PRODUCTION__,
+        liveReloadPort: BUILD_LIVERELOAD_PORT,
+      })
+    );
+  })
+  .get("/bundle.css", async (_, res) => {
+    if (__PRODUCTION__) {
+      res.end(bundlecss);
+    } else {
+      res.end(getFileContents(`${BUILD_DIR}/bundle.css`));
+    }
+  })
+  .get("/bundle.js", async (_, res) => {
+    if (__PRODUCTION__) {
+      res.end(bundlejs);
+    } else {
+      res.end(getFileContents(`${BUILD_DIR}/bundle.js`));
+    }
+  })
+  .get("/iridium", async (_, res) => {
+    const input = getFileContents(args[0]);
     const output = await compile(input, { liveReload: true });
 
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
