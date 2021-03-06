23e754bcef5130be41e8acc8ef89403fe40b55d3
William Lachance <wrlach@gmail.com>
2021-03-06 13:22:52 -0500

    Import compiler from irydium, instead of absolute path in viewer

--- packages/viewer/src/cli.js
+++ packages/viewer/src/cli.js
@@ -1,4 +1,4 @@
-import { compile } from "../../compiler/src/compile.js";
+import { compile } from "@irydium/compiler";
 
 // the following two files are imported as strings, this lets us
 // return them literally
