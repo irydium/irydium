9eb58247e87d3eb30341fb6cbcd1dc406f2af37f
William Lachance <wrlach@gmail.com>
2021-03-06 08:54:59 -0500

    Testing scaffolding

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,8 @@
-module.exports = { presets: ["@babel/preset-env"] };
+module.exports = {
+  presets: ["@babel/preset-env"],
+  env: {
+    test: {
+      plugins: ["@babel/plugin-transform-runtime"],
+    },
+  },
+};
