b26dba88079193505f5d992550f6a87271739926
William Lachance <wrlach@gmail.com>
2021-07-05 15:27:07 -0400

    Throttle updating document to once every 500ms
    Still not great but slightly better than before.

--- packages/site/src/components/Output.svelte
+++ packages/site/src/components/Output.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
+  import { throttle } from "lodash";
   import { compile } from "../../../compiler/src/compile.js";
 
   export let md = "";
   export let title = "Untitled Document";
   let srcdoc = "Loading...";
 
-  $: {
+  const updateDoc = throttle((md) => {
     compile(md).then((output) => {
       srcdoc = output.html;
       title = output.frontMatter.title || "Untitled Document";
     });
+  }, 500);
+
+  $: {
+    updateDoc(md);
   }
 </script>
 
