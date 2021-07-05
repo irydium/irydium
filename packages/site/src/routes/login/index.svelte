c6e302044283a297b454371319b30b54410de89c
William Lachance <wrlach@gmail.com>
2021-07-05 07:54:41 -0400

    Hopefully make login work for real

--- packages/site/src/routes/login/index.svelte
+++ packages/site/src/routes/login/index.svelte
@@ -2,15 +2,25 @@
   import { login } from "../../state/serverActions";
   import { user } from "../../state/sessionStore";
 
+  const isLoggedIn =
+    process.browser &&
+    new URLSearchParams(window.location.search).get("logged_in");
+
   $: {
-    if (!$user) {
-      login("/login");
-    } else {
+    if (!$user && !isLoggedIn) {
+      login("/login?logged_in=1");
+    } else if ($user) {
       window.opener.loginSuccess();
       window.opener.focus();
       window.close();
     }
   }
 </script>
+
+<style>
+  p {
+    padding: 20px;
+  }
+</style>
 
 <p>Logging in...</p>
