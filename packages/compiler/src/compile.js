7d7530890e2105aefc4d2ba53b842f7d57982018
Will Lachance <wrlach@gmail.com>
2021-07-12 17:04:24 -0400

    Fix standalone compiler command (#58)
    Also:
    
    * Don't have compile() itself catch errors (and rethrow). Leave
    that up to the consumer.
    * Clean up rollup config for compiler a bit

--- packages/compiler/src/compile.js
+++ packages/compiler/src/compile.js
@@ -2,14 +2,11 @@ import { mdToSvx } from "./mdToSvx";
 import { svxToHTML } from "./svxToHTML";
 
 export function compile(input, options = {}) {
-  return mdToSvx(input)
-    .then(({ rootComponent, subComponents, frontMatter }) => {
+  return mdToSvx(input).then(
+    ({ rootComponent, subComponents, frontMatter }) => {
       return options.mode === "mdsvex"
         ? { html: rootComponent.code, frontMatter }
         : svxToHTML(rootComponent, subComponents, frontMatter, options);
-    })
-    .catch((err) => {
-      console.error(err);
-      throw err;
-    });
+    }
+  );
 }
