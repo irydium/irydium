e5185a83ac64e8b1086ad8ea530dbdec24018696
William Lachance <wrlach@gmail.com>
2021-07-16 14:17:24 -0400

    Ensure that dev site works without supabase configured

--- packages/site/src/state/sessionStore.ts
+++ packages/site/src/state/sessionStore.ts
@@ -1,9 +1,10 @@
 import { writable } from "svelte/store";
-import { supabase } from "./supabaseClient"
+import { supabase } from "./supabaseClient";
 
 export const user = writable<Object>(false);
 
 export function setupStore() {
+  if (!supabase) return;
   user.set(supabase.auth.user());
   supabase.auth.onAuthStateChange((_, session) => {
     user.set(session && session.user);
