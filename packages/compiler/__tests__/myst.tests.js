7cd1879ef2d68b1d69db1a5747bbfc16d3c5b38a
Maryanne Wachter <maryanne.wachter@gmail.com>
2021-09-30 15:35:07 -0700

    modified panel input

--- packages/compiler/__tests__/myst.tests.js
+++ packages/compiler/__tests__/myst.tests.js
@@ -1,7 +1,7 @@
 import { parsePanel } from "../src/myst.ts";
 
 function createPanel() {
-  let panel = `\`\`\`{panels}\n`;
+  let panel;
   const card1 = createCard("body", "header", "footer");
   const card2 = createCard("body");
   const cards = [card1, card2];
