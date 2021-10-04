e815cfd995850ae42c63a72287c5c4b9db517ca8
Maryanne Wachter <maryanne.wachter@gmail.com>
2021-10-04 13:48:38 -0700

    removed styling for Panels (should be through Bootstrap)

--- packages/compiler/src/templates/Panels.svelte
+++ packages/compiler/src/templates/Panels.svelte
@@ -2,22 +2,6 @@
  </script>
 
 <style>
-  .container {
-	  margin-left: auto;
-	  margin-right: auto;
-	  padding-left: 15px;
-	  padding-right: 15px;
-	  width: 100%;
-  }
-  .row {
-	  display: -ms-flexbox;
-	  display: flex;
-	  -ms-flex-wrap: wrap;
-	  flex-wrap: wrap;
-	  margin-left: -15px;
-	  margin-right: -15px;
-  }
-
 </style>
 
 <div class="container">
