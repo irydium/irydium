3855d2c42a10c68a75b66519a8ec07db130c9133
William Lachance <wlach@protonmail.com>
2021-12-02 18:07:03 -0500

    Take advantage of bootstrap to improve default panel styling
    We'll go with the defaults used by JupyterBook (https://jupyterbook.org/content/content-blocks.html#panels)
    which in turn uses the Sphinx panel extensions: https://sphinx-panels.readthedocs.io/en/latest/#card-layout

--- packages/compiler/src/templates/Card.svelte
+++ packages/compiler/src/templates/Card.svelte
@@ -1,57 +1,18 @@
-<script>
-</script>
-
-<style>
-  .card {
-	  background-clip: border-box;
-	  background-color: #fff;
-	  border: 1px solid rgba(0,0,0,0.125);
-	  border-radius: .25rem;
-	  display: -ms-flexbox;
-	  display: flex;
-	  -ms-flex-direction: column;
-	  flex-direction: column;
-	  min-width: 0;
-	  position: relative;
-	  word-wrap: break-word;
-  }
-
-  .card-header {
-	  background-color: rgba(0,0,0,0.03);
-	  border-bottom: 1px solid rgba(0,0,0,0.125);
-	  margin-bottom: 0;
-	  padding: .75rem 1.25rem;
-  }
-
-  .card-body {
-	  -ms-flex: 1 1 auto;
-	  flex: 1 1 auto;
-	  min-height: 1px;
-	  padding: 1.25rem;
-  }
-
-  .card-footer {
-	  background-color: rgba(0,0,0,0.03);
-	  border-top: 1px solid rgba(0,0,0,0.125);
-	  padding: .75rem 1.25rem;
-  }
-
-</style>
-
-<div>
-  <div class="card">
+<div class="d-flex col-lg-6 col-md-6 col-sm-6 col-xs-12 p-2">
+  <div class="card w-100">
     {#if $$slots.header}
-    <div class="card-header">
-      <slot name="header"></slot>
-    </div>
+      <div class="card-header">
+        <slot name="header" />
+      </div>
     {/if}
     <div class="card-body">
-      <slot name="body"></slot>
+      <slot name="body" />
     </div>
+
     {#if $$slots.footer}
-    <div class="card-footer">
-      <slot name="footer"></slot>
-    </div>
+      <div class="card-footer">
+        <slot name="footer" />
+      </div>
     {/if}
   </div>
 </div>
